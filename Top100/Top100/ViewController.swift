ff2d06023be234efa2af6fbb8accfcffd36832f2
Nick Reichard <reich452@regis.edu>
2019-07-27 17:09:59 -0600

    Made project

---
+++ Top100/Top100/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top100
+//
+//  Created by Nick Reichard on 7/27/19.
+//  Copyright © 2019 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Top100/Top100Tests/Top100Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Top100Tests.swift
+//  Top100Tests
+//
+//  Created by Nick Reichard on 7/27/19.
+//  Copyright © 2019 Nick Reichard. All rights reserved.
+//
+
+import XCTest
+@testable import Top100
+
+class Top100Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
