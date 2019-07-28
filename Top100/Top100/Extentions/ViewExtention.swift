c06e6830935f2f6e81b72f9cc0291e6d99a000bc
Nick Reichard <reich452@regis.edu>
2019-07-27 18:43:24 -0600

    Updated ViewExtention to addopt protocol

--- Top100/Top100/Extentions/ViewExtention.swift
+++ Top100/Top100/Extentions/ViewExtention.swift
@@ -6,4 +6,59 @@
 //  Copyright © 2019 Nick Reichard. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIView: ViewExtensionProtocol {
+    
+    func anchor(top: NSLayoutYAxisAnchor?,
+                paddingTop: CGFloat,
+                bottom: NSLayoutYAxisAnchor?,
+                paddingBottom: CGFloat,
+                left: NSLayoutXAxisAnchor?,
+                paddingLeft: CGFloat,
+                right: NSLayoutXAxisAnchor?,
+                paddingRight: CGFloat,
+                width: CGFloat,
+                height: CGFloat) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top { topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true }
+        
+        if let bottom = bottom { bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true }
+        
+        if let right = right { rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true }
+        
+        if let left = left { leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true }
+        
+        if width != 0 { widthAnchor.constraint(equalToConstant: width).isActive = true }
+        
+        if height != 0 { heightAnchor.constraint(equalToConstant: height).isActive = true }
+    }
+    
+    func anchorWith(top: NSLayoutYAxisAnchor?,
+                paddingTop: CGFloat,
+                bottom: NSLayoutYAxisAnchor?,
+                paddingBottom: CGFloat,
+                left: NSLayoutXAxisAnchor?,
+                paddingLeft: CGFloat,
+                right: NSLayoutXAxisAnchor?,
+                paddingRight: CGFloat,
+                width: CGFloat,
+                greaterHeight: CGFloat) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top { topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true }
+        
+        if let bottom = bottom { bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true }
+        
+        if let right = right { rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true }
+        
+        if let left = left { leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true }
+        
+        if width != 0 { widthAnchor.constraint(equalToConstant: width).isActive = true }
+        
+        if greaterHeight != 0 { heightAnchor.constraint(greaterThanOrEqualToConstant: greaterHeight).isActive = true }
+    }
+}
