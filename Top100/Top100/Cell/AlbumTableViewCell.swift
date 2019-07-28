6e841b55e348892608d69a8f50d68d4ebc5ab297
Nick Reichard <reich452@regis.edu>
2019-07-27 18:37:14 -0600

    Tested AlbumCell

--- Top100/Top100/Cell/AlbumTableViewCell.swift
+++ Top100/Top100/Cell/AlbumTableViewCell.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import UIKit
+
+class AlbumTableViewCell: UITableViewCell {
+    
+    
+    
+    // MARK: - Initailzer
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+   
+}
