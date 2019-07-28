1701081d066194e53a0e79209a29d6753cdbe31f
Nick Reichard <reich452@regis.edu>
2019-07-27 18:49:12 -0600

    Added Color Theme

---
+++ Top100/Top100/Extentions/ColorTheme.swift
@@ -0,0 +1,17 @@
+//
+//  ColorTheme.swift
+//  Top100
+//
+//  Created by Nick Reichard on 7/27/19.
+//  Copyright © 2019 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let mainColor = #colorLiteral(red: 0.1097102091, green: 0.1098582968, blue: 0.1181454733, alpha: 1)
+    static let mainPurple = #colorLiteral(red: 0.5092234612, green: 0.1805469394, blue: 0.9508764148, alpha: 1)
+    static let semiboldWhite = #colorLiteral(red: 0.9488492608, green: 0.9452781081, blue: 0.9700464606, alpha: 1)
+    static let thinWhite = #colorLiteral(red: 0.9487403035, green: 0.9491847157, blue: 0.9698329568, alpha: 1)
+}
